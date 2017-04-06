b6d4a71a10275ffdc1b46f1cd0222d63946de3a8
irdeen <calle_71@hotmail.com>
2017-04-06 11:42:52 +0200

    Added load glove and load csv file functions

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-def loadGloveModel(gloveFile):
+import csv
+import sys
+
+def loadGlove(gloveFile):
     print("Loading Glove Model")
     f = open(gloveFile,'r')
     model = {}
     for line in f:
         splitLine = line.split()
         word = splitLine[0]
-        embedding = [float(val) for val in splitLine[1:]]
+        try:
+            embedding = [float(val) for val in splitLine[1:]]
+        except(ValueError):
+            print(line)
         model[word] = embedding
     print("Done.",len(model)," words loaded!")
+    f.close()
     return model
 
-def loadTrainingData(trainingFile):
+def readCSV(csvfile1):
+    print("Loading csv file")
+    csv.field_size_limit(sys.maxsize)
+    csvfile2 = open(csvfile1, 'r')
+    csvfile3 = csv.reader(csvfile2, delimiter=',', quotechar='|')
+    print("Done loading csv")
+    return csvfile3
 
-    return trainingData
-
-trainingData = loadTrainingData('training.txt')
-model = loadGloveModel('glove.txt')
-print(model['hello'])
+traincsv = readCSV('train.csv')
+glove = loadGlove('glove.840B.300d.txt')
+print(glove['hello'])
