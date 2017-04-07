791c967cf3f790427faa778017d840165a92bc55
Robin Timan <robintiman@gmail.com>
2017-04-07 14:11:35 +0200

    Speller corrected

--- spelling.py
+++ spelling.py
@@ -3,21 +3,20 @@ from enchant.checker import SpellChecker
 
 def __check_spelling(q1, q2):
     dict = enchant.Dict('en_US')
-    chkr = SpellChecker("en_US")
-    chkr.set_text(q1)
+    sent1 = q1.split()
     sent2 = q2.split()
-    for err in chkr:
-    # Check each error
-        if not dict.check(err):
+    for word in sent1:
+        # Check each error
+        if not dict.check(word):
             # If the word exists in the other sentence, it's likely to be correct
-            if err in sent2:
+            if word in sent2:
                 continue
 
             # If not in the other sentence. Check if any of the suggestions is.
-            suggestions = dict.suggest(err)
+            suggestions = dict.suggest(word)
             for s in suggestions:
                 if s in sent2:
-                    q1 = q1.replace(err, s)
+                    q1 = q1.replace(word, s)
     return q1
 
 def correct_spelling(q1, q2):
