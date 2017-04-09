345207723fe8ac12f3d153194d2d871f236611b6
Robin Timan <robintiman@gmail.com>
2017-04-09 11:47:51 +0200

    Working example. Errors exist in conv_to_csv() which appends a new line to the last line. I just removed it manually.
    Also, somewhere along the line a zero vector is added. Haven't
    looked into where that might be.

--- load_files.py
+++ load_files.py
@@ -28,3 +28,26 @@ def load_csv(csvfile1):
     print("Done loading csv file into data list")
     ifile.close()
     return data
+
+
+"""
+Format:
+    test_id,is_duplicate
+    0,0.5
+    1,0.4
+    2,0.9
+    etc.
+"""
+def conv_to_csv(filename):
+    pred = open(filename, 'r')
+    csv = open("predictions.csv", 'w')
+    csv.write("test_id,is_duplicate\n")
+    index = -1
+    for score in pred:
+        if index == -1:
+            index += 1
+            continue
+        csv.write(str(index) + "," + score)
+        index += 1
+    pred.close()
+    csv.close()
