d03b9d99449a3e251ee6857b351712550433e3c4
irdeen <calle_71@hotmail.com>
2017-05-09 20:30:29 +0200

    Remade neural network with keras. Works right now

--- Scripts/neural_network.py
+++ Scripts/neural_network.py
@@ -1,57 +1,48 @@
-import tensorflow as tf
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import *
 
-#Load the vectorized question pairs
-train = np.load("train_vec.npy")
+#Load the vectorized question pairs from train and test
+train = np.load("train_vector.npy")
+test = np.load("test_vector.npy")
 
-#Input tensors of all features x and labels y
-inputX = train[:, 4:]
-inputY = train[:, 0]
+#Separate the vectorized sets into x as features and y as labels
+x_train = train[:, 4:]
+y_train = train[:, 0]
+x_test = test[1:, 1:]
+#y_test = train[:, 4:]
 
-#Hyperparameters
-learning_rate = 0.000001
-training_epochs = 2000
-display_step = 50
-n_samples = len(inputX)
-n_features = len(inputX[0])
-n_labels = 1
+#Initializing hyperparameters
+n_features = len(x_train[0])
+epochs = 1000
+batch_size = 70
+shuffle=True
+dropout_rate = 0 #Helps preventing overfitting
 
-#Feed in the feature inputs
-x = tf.placeholder(tf.float32, [None, n_features])
-#Create weights
-W = tf.Variable(tf.zeros([n_features, n_labels]))
-#Create biases
-b = tf.Variable(tf.zeros([n_labels]))
-#Multiply input by weights and add biases
-y_values = tf.add(tf.matmul(x, W), b)
-#Apply softmax to the values we just created. Softmax is the activation function
-y = tf.nn.softmax(y_values)
-#Feed in the label inputs
-y_ = tf.placeholder(tf.float32, [None])
+#Create neural network model
+model = Sequential()
+model.add(Dense(n_features, input_dim=n_features, activation='softplus'))
+model.add(Dropout(dropout_rate))
+model.add(Dense(n_features, activation='softplus'))
+model.add(Dropout(dropout_rate))
+model.add(Dense(1, activation='sigmoid'))
 
-#Training
-#Create our cost function, mean squared error
-cost = tf.reduce_sum(tf.pow(y_ - y, 2))/(2*n_samples)
-#Gradient descent
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-#Initialize variables and tensorflow session
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-#Training loop
-for i in range(training_epochs):
-    sess.run(optimizer, feed_dict={x: inputX, y_: inputY})
+#Train the model
+model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)
 
-    #Write out logs of training
-    if (i) % 1 == 0:
-        cc = sess.run(cost, feed_dict={x: inputX, y_: inputY})
-        print('Training step:', '%04d' % (i), "cost=", "{:.9f}".format(cc))
+#Test our trained model on the test set
+predictions = model.predict(x_test)
 
-print("Optimization Finished!")
-training_cost = sess.run(cost, feed_dict={x: inputX, y_: inputY})
-print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b))
-
-#Results
-results = sess.run(y, feed_dict={x: inputX})
-print(results)
+#Write the test predictions to submission file
+print("Writing to file...")
+f = open("keras_predictions", 'w')
+f.write("test_id,is_duplicate\n")
+counter = 0
+for val in np.nditer(predictions):
+    f.write(str(counter) + "," + str(val) + "\n")
+    counter += 1
+f.close()
+print("Done!")
