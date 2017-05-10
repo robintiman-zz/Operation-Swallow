27605a296777a92b1146684ad46c11933dbd6cb9
irdeen <calle_71@hotmail.com>
2017-05-10 15:22:57 +0200

    Better parameters again...

--- Scripts/neural_network.py
+++ Scripts/neural_network.py
@@ -26,9 +26,9 @@ x_test = np.nan_to_num(x_test)
 n_features = len(x_train[0])
 epochs = 40
 batch_size = 1000
-dropout_rate = 0.25 #Helps preventing overfitting
+dropout_rate = 0.35 #Helps preventing overfitting
 n_neurons = 2000
-n_hidden_layers = 2
+n_hidden_layers = 4
 shuffle = True
 
 #Create neural network model
@@ -37,8 +37,8 @@ model.add(Dense(n_neurons, input_dim=n_features, activation='relu'))
 model.add(Dropout(dropout_rate))
 for i in range(1, n_hidden_layers):
     #Number of neurons decrease evenly with each layer
-    new_n_neurons = int(n_neurons*((n_hidden_layers-i)/n_hidden_layers))
-    model.add(Dense(new_n_neurons, activation='relu'))
+    n_neurons = int(n_neurons/2)
+    model.add(Dense(n_neurons, activation='relu'))
     model.add(Dropout(dropout_rate))
 model.add(Dense(1, activation='sigmoid'))
 
